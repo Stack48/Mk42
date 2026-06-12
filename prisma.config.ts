938e8ea4473f6eed2f4edd438092cb761131b9a4
Rayane Adjaoud <rayane.adjaoud@gmail.com>
2026-06-12 18:46:05 +0200

    Fix bug après merge
    Correction de certains fichier après le merge

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,11 +5,6 @@ import { defineConfig } from "prisma/config";
 config({ path: ".env" });
 config({ path: ".env.local", override: true });
 
-const databaseUrl = process.env["DATABASE_URL"];
-if (!databaseUrl) {
-  throw new Error("DATABASE_URL must be set");
-}
-
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
